55283245bdb533b57bd7089581d78723534eaa46
C0618C <leopardus@qq.com>
2025-02-10 23:21:14 +0800

    fix: 调整校验规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -66,9 +66,5 @@ module.exports = {
     'no-param-reassign': 0,
     'prefer-regex-literals': 0,
     'import/no-extraneous-dependencies': 0,
-
-    //报错
-    'media-query-no-invalid': 0,
-    'selector-anb-no-unmatchable': 0,
   },
 };
